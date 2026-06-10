863b27d8983e258b60c246e8fc6a3af804e1cd15
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-10 08:39:07 +0500

    Update day3.py

---
+++ day3.py
@@ -0,0 +1,14 @@
+#in this day we learn about comment line and multiples line and also
+# learn escape sequence in python
+
+print("hey iam a good boy");
+print("hello\n new boy");
+# hello how are you
+# right now i am missing 
+#working on hundrad days 
+#hello boy how are you
+# what are you doing 
+# you
+print("hey iam a good boy \"and also the viewer is also a good boy/girl\"")
+print("my aim to learn python\'and become expert in python\'");
+print("hey ",7,"book",5, sep="---",end="009");
