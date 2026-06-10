0cd136cd82422dd71499d3a7ed83f37f7f5c7198
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-10 11:30:53 +0500

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,19 @@
+a = 12312342
+a1 = 23222
+print(a)
+b = "Qurban"
+print(b)
+c = True
+d = 1.21
+print(c)
+print(a1+a)
+print("the data type of a is ",type(a))
+print("the data type of b is ",type(b))
+print("the data type of c is ",type(c))
+print ("the data type of d is", type (d))
+list = [1,2,3,4],["apple ","banana"  ,"mango"],[True,False,True]
+print(list)
+dict1 = {"name": "Qurban","age":20, "class": "BS","can vote":True}
+print(dict1)
+print (type(dict1))
+tuple1 = (23,"Qurban","ali")
