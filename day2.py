5fda1292e37ad73cfb2f059345cc5c7389bd9e1e
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-10 07:38:16 +0500

    add

---
+++ day2.py
@@ -0,0 +1,9 @@
+print("Hello World!",7,6,6+8,"how are you ");
+print("What is your name ?");
+print(23*54);
+print(100/2.90);
+print(100//2.49);
+print(100%2.49);
+print(2**3);
+print("The value of 2 to the power 3 is ",2**3);
+print("The value of 7 to the power 9 is ",pow(7,9));
