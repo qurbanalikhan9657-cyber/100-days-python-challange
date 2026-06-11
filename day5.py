80ab7248c44b12de5e80ac144efab43c6ee34d56
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-11 20:57:39 +0500

    day5 code

---
+++ day5.py
@@ -0,0 +1,23 @@
+print(5*5)
+print(5%3)
+print(7//2)
+print(7%2)
+print(5**12)
+print(2**5)
+a = 50;
+b = 5;
+print("The addition is:" , a , "+", b,"is:",a + b)
+print("The substriction is:", a , "-", b,"is:", a - b)
+print("The multiplication is:", a , "*", b,"is:", a * b)
+print("The divison is:", a , "/", b,"is:", a / b)
+
+# print("The addition of a and b is", a,"+"b, a + b)
+# print("===calculator in python take input and perform all operation ===")
+# num1 = float(input("Enter you first number:"))
+# input(num1)
+# num2 = float(input("Enter you second number:"))
+# print("The addition of ", num1 , "and" , num2 , "is", a+b)
+int = num1
+print("Enter a number:")
+input(num1)
+print("num1 is :", num1)
