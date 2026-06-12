4f251329acaf9cd0124a5dab2adc7ed35673833f
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-12 18:38:50 +0500

    day6.py

---
+++ day6.py
@@ -0,0 +1,22 @@
+#today lesson is about TypeCasting
+a = 4;
+b = 9;
+c = "4";
+d = "2";
+e = "45";
+f = "12.34";
+#convering string into intger
+#Ecplicit Typecasting
+print(int(c)+ int(d))
+print(int(e) + b)
+print(a+b)
+print(c+d)
+# print(int(f)+a)
+print(float(f) + b)
+#implicit typecasting
+g = 5.5;
+h = 5;
+i = 0.4543;
+j = 34;
+print(i+j)
+print(g+h)
---
+++ day8.py
@@ -0,0 +1,15 @@
+#This file is about our excrise of pervious lec 
+#we are taking input from user and we are performing the 
+#all arthematic operations lets get started
+x = input("Please Enter your good name ")
+print("WELCOME MR",x)
+print("********ARETHMATIC OPERATIONS********")
+a = input("Enter your first number :")
+b = input("Enter your second number :")
+print("The sum of ",a,"and", b , "is :",int(a)+int(b))
+print("The diffrence of ",a, "and",b, "is: ", int(a)-int(b))
+print("The multiple of ",a,"and", b , "is: ",int(a)*int(b))
+print("The diff of ",a,"and", b , "is: ",int(a)/int(b))
+print("The fraction of ",a,"and", b , "is: ",int(a)//int(b))
+print("The modlous of ",a,"and", b , "is: ",int(a)%int(b))
+
