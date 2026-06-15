b6458bb544b01aeea8c3c59f42364569773fd74f
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-15 17:27:29 +0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -4,11 +4,9 @@
 print("hey iam a good boy");
 print("hello\n new boy");
 # hello how are you
-# right now i am missing 
 #working on hundrad days 
 #hello boy how are you
 # what are you doing 
-# you
 print("hey iam a good boy \"and also the viewer is also a good boy/girl\"")
 print("my aim to learn python\'and become expert in python\'");
 print("hey ",7,"book",5, sep="---",end="009");
