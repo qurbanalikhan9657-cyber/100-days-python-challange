cbf789c4acdad744f2a6440e73f3fb477ad96924
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-15 17:27:47 +0500

    update_day_11

---
+++ day11.py
@@ -0,0 +1,57 @@
+#today our lec about string slicing
+name = "Qurban, Khan"
+print(name[0:6])
+print(len(name))
+print(type(name))
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+num = "12345678910111213"
+for i  in alphabet:
+    print(i)
+print("for character in alphabet")
+for j in num :
+    print(j)
+fruit1= "mango"
+len1 = len(fruit1)
+
+fruit2 = "orange"
+len2 = len(fruit2)
+
+fruit3 = "banana"
+len3 = len(fruit3)
+
+fruit4 = "pineapple"
+len4 = len(fruit4)
+
+fruit5 = "stawberry"
+len5= len(fruit5)
+
+string = "my name is qurban  khan and im learning python it is my 10th day"
+len6 = len(string)
+for character in name:
+    print(character)
+print("mango is a ",len1,"character word")
+print("orange is a ",len2,"character word")
+print("banana is a ",len3, fruit3[0:5],"character word")
+print("pineapple is a ",len4,"character word")
+print ("the length of fruit 5 is ",len5)
+print("the length of string is ",len6)
+q= "ali"
+print(len(q))
+
+print("****************************************")
+veg = "kaddu"
+print(veg[0:3])
+print(len(veg[0:3]))
+print((veg[2:5]))
+print((veg[0:6]))
+print("*****************************************")
+veg1 = "laddyFinger"
+for character in veg1:
+    print(character)
+for i in veg1:
+    print(i) 
+#Quiz
+print("###############################################")
+nm = "Qurban"
+print(nm[-4:-2])
