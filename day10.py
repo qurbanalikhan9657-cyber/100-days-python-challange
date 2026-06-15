d54592798c41cf129c15f9ce56723fad1b4c195e
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-15 17:28:09 +0500

    update_day_10

---
+++ day10.py
@@ -0,0 +1,20 @@
+#this is the practince day about what i learn in pervious days
+#take input from user
+x = input("Enter your name please:")
+print("You enter your name and your name is:",x)
+#type casting 
+a = input("enter a num1:")
+b = input("enter num2:")
+c = 22.22
+d = 10
+name = "my name is qurban khan and iam in learning phase of python"
+print(a+b)
+print ("output by type casting")
+print(int (a)+ int(b))
+print(c+d)
+print("we can get output of our varibale datatype")
+print (type(c))
+for character in name:
+   print(character)
+print(type (c))
+
