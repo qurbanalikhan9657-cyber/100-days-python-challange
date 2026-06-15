c887d0009067e3d566ff61b48fde51941f21ccc5
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-15 17:27:11 +0500

    update_day_9

---
+++ day9.py
@@ -0,0 +1,30 @@
+#today our lecture is about string
+name = "Qurban"
+friend = "Abdullah"
+anotherFriend = 'Mustafa'
+banana = "he said,\"He want eat banana\""
+apple = 'He said, "he want to eat an apple "'
+orange = """ Hello Qurban , how are you 
+what are you doing right now
+he said come to me i wana meet you """
+print("hello\n"+name)
+print(apple)
+print(banana)
+print(orange)
+#Quran
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(banana[6])
+print(banana[7])
+print("lets use a for loop\n")
+for character in apple:
+    print(character)
+print("Now this time in want to print name by using for loop\n")
+for character in name:
+        print(character)
+print(name[4])
+print(type())
