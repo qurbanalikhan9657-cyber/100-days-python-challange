a0e5acfb338944e91c143c376b30c18ef5eb5eab
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-24 13:24:11 +0500

    Create day18.py
    update_day18

---
+++ day18.py
@@ -0,0 +1,76 @@
+i = int(input("Enter a number you want to start counting from:"))
+j = int(input("enter till you want counting"))
+while (i<=j):
+ print(i)
+ i =i+1
+print("done with loop")
+ 
+i = int (input("Enter a number"))
+while(i<10):
+ i = int (input("Enter a number"))
+ print(i)
+
+x =50
+while(x<=0):
+ print(i)
+ i=i-1
+count = 5
+while(count>0):
+    print(count)
+    count = count-1
+else:
+    print("iam inside else")
+a =0
+while (a<=10):
+    print("How are you\n ")
+    a=a+1
+for i in range(8):
+    print("HELLO WORLD")
+i = int(input("Enter a number:"))
+while (i>=10):
+ print(i,"is not require number TRY AGAIN!!!!")
+ i = int(input("Enter a number:"))
+ print("Require matched")
+i = 1
+while (i<=20):
+    print(i,": I am using while loop to print following ")
+    i = i+1
+i = int(input("Enter a postive number"))
+while(i<0):
+    print("you have enter a Negitive number")
+    i = int(input("Please Enter a postive number"))
+print("You have entered positve number")
+while True:
+ number = int(input("Enter a number for complete division by 2\n"))
+ if (number==0):
+     print("THANk You")
+     break
+ 
+ while number>2:
+    number=number/2
+    print(f"current number is:{number}")
+while True:
+    number = int(input("Enter a number for complete division by 2 (or 0 to exit)\n"))
+    
+    if number == 0:
+        print("THANK You")
+        break
+        
+    while number > 2:
+        number = number / 2
+        print(f"current number is:{number}")
+
+
+while True:
+    number = int (input("Enter a number to complete divison by to or (0 to get exit) \n"))
+    if number==0:
+        print("THANK YOU")
+        break
+    
+    while number>2:
+        number= number/2
+        print(f"current number is {number}")
+
+
+
+
