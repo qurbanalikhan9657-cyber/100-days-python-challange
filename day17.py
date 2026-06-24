e42d0014367344699d3f20851192daadf6158a33
qurbanalikhan9657 <qurbankhan9657@gmail.com>
2026-06-24 13:23:49 +0500

    update-day17

---
+++ day17.py
@@ -0,0 +1,44 @@
+name = 'Qurban'
+for i in name:
+ print(i)
+ if(i=='b'):
+  print("this is something special")
+
+colors = ["Red ","Green","blue","orange"]
+for x in  colors:
+ print(x)
+ for i in x:
+  print(i)
+color = ["red","green","yellow"]
+for i in color:
+  print(i)
+  for x in i :
+   print(x)
+
+for k in range (6):
+ print(k+1)
+for c in range(1,20):
+ print(c+1)
+
+for a in range (20):
+ print(a)
+ for b in range(90):
+  print(b)
+
+name = " irfan"
+for d in range(50):
+ print(name)
+ for d in name:
+  print(d)
+for k in range(0,1000,15):
+ print(k)
+name = ":I LOVE PAKISTAN "
+for x in range(500):
+    print(x,name)
+
+
+fruits =["banana","apple","date","mango"]
+for j in range(5):
+ for i in fruits:
+  print(j,i)
+ 
